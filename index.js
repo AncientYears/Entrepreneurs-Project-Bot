07a1746fbeb634157a282706a5f634dd9e884388
AncientYT <rlichota66@gmail.com>
2018-09-21 20:26:00 +0100

    Added ready event

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const discord = require("discord.js")
+const client = new discord.Client({disableEveryone: false})
+
+client.on("ready", async () => {
+    console.log(`${client.user.username} is up and running!`)
+  })
