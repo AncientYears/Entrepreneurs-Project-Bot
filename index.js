2cf57a83e0e7ec2d891c3c6fe4e7cb6aa8d18f97
AncientYT <rlichota66@gmail.com>
2018-09-23 14:20:11 +0100

    First database test

--- index.js
+++ index.js
@@ -5,4 +5,19 @@ client.on("ready", async () => {
     console.log(`${client.user.username} is up and running!`)
   })
 
+  var ecoCon = mysql.createConnection({
+    host: process.env.mysqlHost,
+    user: process.env.mysqlUser,
+    password: process.env.mysqlPassword,
+    database: process.env.mysqlDatabase
+});
+
+ecoCon.connect(err => {
+  if(err) {
+  console.error(err)
+  } else {
+console.log("Connected to Economy Database!"); 
+}
+})
+
   client.login(process.env.TOKEN)
