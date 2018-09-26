6eb39d1daf8c052984d43f49b589a027f729f894
AncientYT <rlichota66@gmail.com>
2018-09-26 16:23:29 +0100

    First Command Run #3

--- commands/ping.js
+++ commands/ping.js
@@ -2,8 +2,8 @@ const discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
     let pingEmbed = new discord.RichEmbed()
-    .setAuthor("Bot Latency", bot.user.avatarURL)
-    .addField("Ping", bot.ping)
+    .setAuthor("Bot Latency", client.user.avatarURL)
+    .addField("Ping", client.ping)
 }
 
 module.exports.help = {
