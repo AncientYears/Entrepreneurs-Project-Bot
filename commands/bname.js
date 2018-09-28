972f2b4f5224102d28ad8d30014a960ac01f09e7
AncientYT <rlichota66@gmail.com>
2018-09-29 00:04:10 +0100

    Is this too many commits?

--- commands/bname.js
+++ commands/bname.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args, ecoPool) => {
 	ecoPool.getConnection(function(err, connection) {
-		connection.query('SELECT * FROM stats', function(error, results, fields) {
+		connection.query(`SELECT * FROM stats WHERE userID = '${message.author.id}'`, function(error, results, fields) {
 			if(!results[0].businessName) {
 				if(!args.join(' ')) return message.reply('Include a name.');
 				connection.query(`UPDATE stats SET businessName = '${args.join(' ')}' WHERE userID = '${message.author.id}'`);
--- index.js
+++ index.js
@@ -15,17 +15,9 @@ client.on('message', async (message) => {
 	if(message.author.bot) return;
 
 	ecoPool.getConnection(function(err, connection) {
-		connection.query('SELECT * FROM stats', function(error, results, fields) {
-			if(!results[0]) {
-				connection.query(`INSERT IGNORE INTO stats (userID, businessName, businessType, cash, bank, netWorth, employees, stocks) VALUES ('${message.author.id}', '', '', ${0}, ${0}, ${0}, ${0}, ${0})`)    
-				connection.release();
-				if (error) throw error;
-			}
-			else {
-				connection.release();
-				if (error) throw error;
-			}
-		});
+		connection.query(`INSERT IGNORE INTO stats (userID) VALUES ('${message.author.id}')`);
+		connection.release();
+		if (err) throw err;
 	});
 
 	const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|\\${prefix})\\s*`);
@@ -45,7 +37,7 @@ client.on('guildMemberAdd', (member) => {
 	ecoPool.getConnection(function(err, connection) {
 		connection.query('SELECT * FROM stats', function(error, results, fields) {
 			if(!results[0].userID) {
-				connection.query(`INSERT IGNORE INTO stats (userID, cash, bank, netWorth, employees, stocks) VALUES ('${member.id}', ${0}, ${0}, ${0}, ${0}, ${0})`);
+				connection.query(`INSERT IGNORE INTO stats (userID) VALUES ('${member.id}')`);
 				connection.release();
 				if (error) throw error;
 				member.user.send(`
