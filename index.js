12992f7dac710f313c2ebdec1aa1efa68d595e52
AncientYT <rlichota66@gmail.com>
2018-09-29 00:10:53 +0100

    Pls say this is the last commit

--- index.js
+++ index.js
@@ -15,9 +15,17 @@ client.on('message', async (message) => {
 	if(message.author.bot) return;
 
 	ecoPool.getConnection(function(err, connection) {
-		connection.query(`INSERT IGNORE INTO stats (userID) VALUES ('${message.author.id}')`);
-		connection.release();
-		if (err) throw err;
+		connection.query(`SELECT * FROM stats WHERE userID = ${message.author.id}`, function(error, results, fields) {
+			if(!results[0]) {
+				connection.query(`INSERT IGNORE INTO stats (userID, businessName, businessType, cash, bank, netWorth, employees, stocks) VALUES ('${message.author.id}', '', '', ${0}, ${0}, ${0}, ${0}, ${0})`)    
+				connection.release();
+				if (error) throw error;
+			}
+			else {
+				connection.release();
+				if (error) throw error;
+			}
+		});
 	});
 
 	const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|\\${prefix})\\s*`);
@@ -35,9 +43,9 @@ client.on('message', async (message) => {
 
 client.on('guildMemberAdd', (member) => {
 	ecoPool.getConnection(function(err, connection) {
-		connection.query('SELECT * FROM stats', function(error, results, fields) {
+		connection.query(`SELECT * FROM stats WHERE userID = ${member.id}`, function(error, results, fields) {
 			if(!results[0].userID) {
-				connection.query(`INSERT IGNORE INTO stats (userID) VALUES ('${member.id}')`);
+				connection.query(`INSERT IGNORE INTO stats (userID, cash, bank, netWorth, employees, stocks) VALUES ('${member.id}', ${0}, ${0}, ${0}, ${0}, ${0})`);
 				connection.release();
 				if (error) throw error;
 				member.user.send(`
