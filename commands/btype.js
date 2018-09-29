0fe2a812ba318132a5a60165b1dee5ced1ade089
AncientYT <rlichota66@gmail.com>
2018-09-29 16:22:02 +0100

    Format updates

--- commands/btype.js
+++ commands/btype.js
@@ -10,13 +10,31 @@ That is an invalid business type.
 
 The types are as follows:\`\`\`css
 [ Farm ] 
-- You are able to produce raw goods, e.g fruits, vegetables, meat, dairy products, etc.
+# You are able to produce raw goods, e.g fruits, vegetables, meat, dairy products, etc.
 [ Advantages ]
-- No need to constantly buy stocks from other companies
-- Companies rely on your materials, meaning you can set the prices
+# No need to constantly buy stocks from other companies
+# Companies rely on your materials, meaning you can set the prices
 [ Disadvantages ]
-- Your profits depend on quality of crops, season, weather etc.
-- You have limited access to other companies so trade might be a bit difficult\`\`\`   
+# Your profits depend on quality of crops, season, weather etc.
+# You have limited access to other companies so trade might be a bit difficult\`\`\`   
+\`\`\`css
+[ Factory ]
+# You are able to buy raw goods from farms and process them to create something new
+[ Advantages ]
+# You can sell the new items you produce for more profit
+# No limit to what items you can produce
+[ Disadvantages ]
+# You depend on farms and other raw material producers for profit
+# You can lose money when demand drops for your product\`\`\`
+\`\`\`css
+[ Shop ]
+# You buy stock for cheap and sell it for more to make a profit.
+[ Advantages ]
+# You can buy stock from both raw-material producers and factories
+# You can advertise your shop to get more people to buy your stock
+[ Disadvantages ]
+# You may have to lower the price for customers to buy a certain item
+# You are limited in wether/season conditions due to the fact that you need to buy stock from other companies. 
 `) && connection.release();
 				}
 				else {
