cea5c3f6a82106b4b14a4e82d92a9c8b8c829f42
PLASMAchicken <plasmachickenmc@gmail.com>
2018-10-03 18:46:59 +0200

    eslint

--- commands/eval.js
+++ commands/eval.js
@@ -1,26 +1,24 @@
-const discord = require('discord.js');
 const cblockre = /(^```js)|(```$)/g;
 module.exports.run = async (client, message, args, ecoPool) => {
-    if (!['193406800614129664', '211795109132369920'].includes(message.author.id)) return message.reply('This command cannot be used by you!');
-        try {
-            let content = args.join(' ');
-            if (cblockre.test(content)) {
-                content = content.replace(cblockre, '').trim();
-            }
-            let evaled = eval(content);
-            if (typeof evaled !== 'string') {
-                evaled = require('util').inspect(evaled);
-            }
-            message.channel.send(evaled)
-        }
-        catch (err) {
-            message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
-    }
+	if (!['193406800614129664', '211795109132369920'].includes(message.author.id)) return message.reply('This command cannot be used by you!');
+	try {
+		let content = args.join(' ');
+		if (cblockre.test(content)) {
+			content = content.replace(cblockre, '').trim();
+		}
+		let evaled = eval(content);
+		if (typeof evaled !== 'string') {
+			evaled = require('util').inspect(evaled);
+		}
+		message.channel.send(evaled);
+	}
+	catch (err) {
+		message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
+	}
 };
 module.exports.help = {
-    name: 'eval'
-    , description: 'Evals'
-    , usage: 'eval <<>>'
-    , aliases: ['e']
-    , hideinhelp: true
-, };
+	name: 'eval',
+	description: 'Evals',
+	usage: 'eval <<>>',
+	aliases: ['e'], hideinhelp: true,
+};
