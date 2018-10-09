eb361df08994d44f69ce1321fba13f13aca58d35
AncientYT <rlichota66@gmail.com>
2018-10-09 21:43:44 +0100

    First run of buy buy buy

--- commands/buy.js
+++ commands/buy.js
@@ -1,7 +1,8 @@
 const discord = require('discord.js');
 
 module.exports.run = async (client, message, args, ecoPool) => {
-	if(!args[0]) {
+	const tobuy = args.shift().toLowerCase();
+	if(!tobuy) {
 		const categoryEmbed = new discord.RichEmbed()
 			.setAuthor('Categories', message.author.displayAvatarURL)
 			.setDescription(`
@@ -14,7 +15,7 @@ module.exports.run = async (client, message, args, ecoPool) => {
 			.setFooter('?buy <category> to view a category');
 		message.channel.send(categoryEmbed);
 	}
-	else if (args[0].toLowerCase() === 'farm') {
+	else if (tobuy.toLowerCase() === 'farm') {
 		const farmEmbed = new discord.RichEmbed()
 			.setAuthor('Farm', message.author.displayAvatarURL)
 			.setDescription(`
@@ -24,9 +25,20 @@ module.exports.run = async (client, message, args, ecoPool) => {
 			.setFooter('?buy <item> <amount> to purchase an item');
 		message.channel.send(farmEmbed);
 	}
-	else if (args[0].toLowerCase() === 'potato') {
+	else if (tobuy.toLowerCase() === 'potato') {
 		if(isNaN(args[1])) return message.channel.send('How many potatoes do you wanna buy? **?buy potato <amount>**');
-		message.channel.send('So you wanna buy a potato, huh? Well too bad the devs gotta make this part of the bot work first');
+		if(args[1].startsWith('-')) return message.channel.send('Hey, you cannot buy negative potato(es)!');
+		ecoPool.getConnection(function(err, connection) {
+			connection.query(`SELECT * FROM stats WHERE userID = '${message.author.id}'`, function(error, results, fields) {
+				if(results[0].cash < (1 * args[1])) return message.channel.send('You do not have enough cash to buy this!') && connection.release();
+				const resultedArray = results[0].stocks;
+				resultedArray[0] = args[1];
+				connection.query(`UPDATE stats SET stocks = '${resultedArray}' WHERE userID = '${message.author.id}'`);
+				connection.query(`UPDATE stats SET cash = '${results[0].cash - (1 * args[1])}' WHERE userID = '${message.author.id}'`);
+				message.channel.send('You have successfully bought **' + args(1) + '** potato(es) \nThis has costed you **' + results[0].cash - (1 * args[1] + '**!'));
+				connection.release();
+			});
+		});
 	}
 };
 
