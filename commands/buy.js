ee299be4ba9cd486dc0e4f02953dcb425d60d82f
AncientYT <rlichota66@gmail.com>
2018-10-13 10:50:26 +0100

    Potato!

--- commands/buy.js
+++ commands/buy.js
@@ -31,7 +31,7 @@ module.exports.run = async (client, message, args, ecoPool) => {
 			connection.query(`SELECT * FROM stats WHERE userID = '${message.author.id}'`, function(error, results, fields) {
 				if(results[0].cash < (1 * Number(args[1]))) return message.channel.send('You do not have enough cash to buy this!') && connection.release();
 				const resultedArray = results[0].stocks;
-				resultedArray.push(args[1]);
+				resultedArray.push(args[1] + ' potato');
 				connection.query(`UPDATE stats SET stocks = '${resultedArray}' WHERE userID = '${message.author.id}'`);
 				connection.query(`UPDATE stats SET cash = '${results[0].cash - (1 * Number(args[1]))}' WHERE userID = '${message.author.id}'`);
 				message.channel.send('You have successfully bought **' + args[1] + '** potato(es) \nThis has costed you **' + results[0].cash - (1 * Number(args[1]) + '**!'));
