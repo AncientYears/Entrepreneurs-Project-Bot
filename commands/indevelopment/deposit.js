dcfea28fe3f4830a91d95f4398f24eb9df4b1d44
AncientYT <rlichota66@gmail.com>
2018-10-14 20:13:52 +0100

    Deposit

---
+++ commands/indevelopment/deposit.js
@@ -0,0 +1,19 @@
+const discord = require('discord.js');
+
+module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
+	if(!args[0]) return message.channel.send('Include a number!\n**?deposit <amount>**');
+	if(isNaN(args[0])) return message.channel.send('Invalid Number.');
+	if(args[0] <= 0) return message.channel.send('You cannot deposit negative money!');
+	if(stats.cash < args[0]) return message.channel.send('You do not have that amount of money!');
+
+	connection.query(`UPDATE stats SET bank = '${Number(stats.bank) + Number(args[0])}' WHERE userID = '${message.author.id}'`);
+	connection.query(`UPDATE stats SET cash = '${Number(stats.cash) - Number(args[0])}' WHERE userID = '${message.author.id}'`);
+
+	message.channel.send(`You have successfully deposited **${Number(args[0])}** into your bank! \nYou have **${Number(stats.cash) - Number(args[0])}** cash left!`);
+};
+
+module.exports.help = {
+	name: 'deposit',
+	aliases: ['dep', 'd'],
+	hideinhelp: false,
+};
--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
 	const pingEmbed = new discord.RichEmbed()
 		.setAuthor('Bot Latency', client.user.avatarURL)
-		.addField('Ping', client.ping);
+		.addField('Ping', Math.round(client.ping) + 'ms');
 
 	message.channel.send(pingEmbed);
 };
