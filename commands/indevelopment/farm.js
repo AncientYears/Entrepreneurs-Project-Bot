02f54b80c101a73980dbbfb14a39c0b877365f97
AncientYT <rlichota66@gmail.com>
2018-10-14 21:49:22 +0100

    upd

--- commands/indevelopment/farm.js
+++ commands/indevelopment/farm.js
@@ -2,7 +2,8 @@ const discord = require('discord.js');
 
 module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
 	if(stats.businessType !== 'farm') return message.channel.send('Sorry, you do not have a farm! \nYou have a **' + stats.businessType + '**');
-	const timeLeft = stats.TimeLength - message.createdTimestamp;
+	const timeLeftStamp = stats.TimeLength - message.createdTimestamp;
+	const timeLeft = new Date(timeLeftStamp);
 
 	const farmEmbed = new discord.RichEmbed()
 		.setAuthor('Farm', message.author.displayAvatarURL)
