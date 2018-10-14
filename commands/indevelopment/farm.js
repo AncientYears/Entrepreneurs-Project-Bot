206ec331875b2d39d9cac2dbf6d859e2cff0b39c
AncientYT <rlichota66@gmail.com>
2018-10-14 22:03:47 +0100

    nope

--- commands/indevelopment/farm.js
+++ commands/indevelopment/farm.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args, ecoPool, connection, stats) =
 		.addField('Status', stats.TimeLength !== 0 ? 'Not Growing' : `${timeLeft.getHours()}h ${timeLeft.getMinutes()}m ${timeLeft.getSeconds()}s`)
 		.setDescription(`
 **Crops Available**
-- **${stats.stocks.potato + '**' || 'no'} potato
+- **${stats.stocks.potato || '**no'} potato
 `)
 		.setFooter('Plant your crops using ?plant!');
 
