2328d70a98ebe32ada638b2ce81ab6334dbb9213
AncientYT <rlichota66@gmail.com>
2018-10-14 19:21:11 +0100

    Withdraw command!

---
+++ commands/indevelopment/withdraw.js
@@ -0,0 +1,19 @@
+const discord = require('discord.js');
+
+module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
+	if(!args[0]) return message.channel.send('Include a number!\n**?withdraw <amount>**');
+	if(isNaN(args[0])) return message.channel.send('Invalid Number.');
+	if(args[0] <= 0) return message.channel.send('You cannot withdraw negative money!');
+	if(stats.bank < args[0]) return message.channel.send('You do not have that amount of money inside of your bank!');
+
+	connection.query(`UPDATE stats SET bank = '${stats.bank - args[0]}' WHERE userID = '${message.author.id}'`);
+	connection.query(`UPDATE stats SET cash = '${stats.cash + args[0]}' WHERE userID = '${message.author.id}'`);
+
+	message.channel.send('You have successfully withdrawn **' + args[0] + '** from your bank! \nYou have **' + stats.bank - args[0] + '** left inside your bank!');
+};
+
+module.exports.help = {
+	name: 'withdraw',
+	aliases: ['wd', 'with'],
+	hideinhelp: false,
+};
