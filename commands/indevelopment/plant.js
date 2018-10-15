19acacbabb9d6965200f9de420baf5094735a5a3
AncientYT <rlichota66@gmail.com>
2018-10-15 17:28:46 +0100

    Swaparro

--- commands/indevelopment/plant.js
+++ commands/indevelopment/plant.js
@@ -3,10 +3,7 @@ const discord = require('discord.js');
 
 module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
 	if(stats.businessType !== 'farm') return message.channel.send('Sorry, you do not have a farm! \nYou have a **' + stats.businessType + '**');
-	if(stats.TimeLength !== 0) {
-		message.channel.send('Your crops are already growing, use the **?farm** command to view information about them!');
-	}
-	else {
+	if(stats.TimeLength === 0) {
 		if(!args[0] && args[0] !== stats.stocks.potato) return message.channel.send('You do no have this crop! \n**?plant <crop> <amount>');
 		if(args[0] === 'potato' && !stats.stocks.potato && stats.stocks.potato <= 1) return message.channel.send('You do not have any potatoes, please go buy some \n**?buy**');
 		if(isNaN(args[1])) return message.channel.send('Invalid Number! \n**?plant <crop> <amount>');
@@ -22,6 +19,10 @@ Successfully planted **${args[1]} ${args[0]}**!
 			.setColor('GREEN')
 			.setFooter('Use the ?farm command to view information about your crop!');
 		message.channel.send(farmEmbed);
+	}
+	else {
+		message.channel.send('Your crops are already growing, use the **?farm** command to view information about them!');
+
 	}
 };
 
