76d5e38dd339f81a4e83705ae329c5380359a76b
PLASMAchicken <plasmachickenmc@gmail.com>
2018-10-16 22:52:12 +0200

    Update farm.js

--- commands/indevelopment/farm.js
+++ commands/indevelopment/farm.js
@@ -2,12 +2,12 @@ const discord = require('discord.js');
 
 module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
 	if(stats.businessType !== 'farm') return message.channel.send('Sorry, you do not have a farm! \nYou have a **' + stats.businessType + '**');
-	const timeLeftStamp = message.createdTimestamp - stats.timeLength ;
+	const timeLeftStamp = stats.timeLength - message.createdTimestamp;
 	const timeLeft = new Date(timeLeftStamp);
 
 	const farmEmbed = new discord.RichEmbed()
 		.setAuthor('Farm', message.author.displayAvatarURL)
-		.addField('Status', stats.timeLength !== 0 ? 'Not Growing' : `${timeLeft.getHours()}h ${timeLeft.getMinutes()}m ${timeLeft.getSeconds()}s`)
+		.addField('Status', stats.timeLength === 0 ? 'Not Growing' : `${timeLeft.getHours()}h ${timeLeft.getMinutes()}m ${timeLeft.getSeconds()}s`)
 		.setDescription(`
 **Crops Available**
 - ${stats.stocks.potato || 'no'} potato
