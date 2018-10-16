35c0c9244d3cb741410c54f818e97fa32ab7b833
PLASMAchicken <plasmachickenmc@gmail.com>
2018-10-16 22:45:27 +0200

    Update farm.js

--- commands/indevelopment/farm.js
+++ commands/indevelopment/farm.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 
 module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
 	if(stats.businessType !== 'farm') return message.channel.send('Sorry, you do not have a farm! \nYou have a **' + stats.businessType + '**');
-	const timeLeftStamp = stats.timeLength - message.createdTimestamp;
+	const timeLeftStamp = message.createdTimestamp - stats.timeLength ;
 	const timeLeft = new Date(timeLeftStamp);
 
 	const farmEmbed = new discord.RichEmbed()
