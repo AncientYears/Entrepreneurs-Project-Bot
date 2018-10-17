10828e7251f831a020c324c463114b76259aab2f
AncientYT <rlichota66@gmail.com>
2018-10-17 14:44:44 +0100

    temporary fix

--- commands/indevelopment/plant.js
+++ commands/indevelopment/plant.js
@@ -7,11 +7,11 @@ module.exports.run = async (client, message, args, ecoPool, connection, stats) =
 		message.channel.send('Your crops are already growing, use the **?farm** command to view information about them!');
 	}
 	else {
-		if(!args[0] && args[0] !== stats.stocks.potato) return message.channel.send('You do no have this crop! \n**?plant <crop> <amount>');
-		if(args[0] === 'potato' && !stats.stocks.potato && stats.stocks.potato <= 1) return message.channel.send('You do not have any potatoes, please go buy some \n**?buy**');
+		if(!args[0] /* && args[0] !== stats.stocks.potato/*/) return message.channel.send('You do no have this crop! \n**?plant <crop> <amount>');
+		if(args[0] === 'potato' && !stats.stocks.potato && stats.stocks.potato <= 0) return message.channel.send('You do not have any potatoes, please go buy some \n**?buy**');
 		if(isNaN(args[1])) return message.channel.send('Invalid Number! \n**?plant <crop> <amount>');
 
-		connection.query(`UPDATE stats SET stocks = '${Number(stats.stocks.potato) - Number(args[1])}' WHERE userID = '${message.author.id}'`);
+		connection.query(`UPDATE stats SET stocks = '${Number(stats.stocks.args[0]) - Number(args[1])}' WHERE userID = '${message.author.id}'`);
 		connection.query(`UPDATE stats SET creation = '${Number(args[1])}' WHERE userID = '${message.author.id}'`);
 		connection.query(`UPDATE stats SET timeLength = '${Number(message.createdTimestamp) + Number(ms('1h'))}' WHERE userID = '${message.author.id}'`);
 
