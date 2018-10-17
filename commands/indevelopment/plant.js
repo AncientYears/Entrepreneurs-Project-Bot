5886da6fc238a6f74bc2c392d3ac03aaa36679d1
AncientYT <rlichota66@gmail.com>
2018-10-17 14:39:24 +0100

    potato temp fix

--- commands/indevelopment/plant.js
+++ commands/indevelopment/plant.js
@@ -11,7 +11,8 @@ module.exports.run = async (client, message, args, ecoPool, connection, stats) =
 		if(args[0] === 'potato' && !stats.stocks.potato && stats.stocks.potato <= 1) return message.channel.send('You do not have any potatoes, please go buy some \n**?buy**');
 		if(isNaN(args[1])) return message.channel.send('Invalid Number! \n**?plant <crop> <amount>');
 
-		connection.query(`UPDATE stats SET creation = '${Number(stats.creation) - Number(args[1])}' WHERE userID = '${message.author.id}'`);
+		connection.query(`UPDATE stats SET stocks = '${Number(stats.stocks.potato) - Number(args[1])}' WHERE userID = '${message.author.id}'`);
+		connection.query(`UPDATE stats SET creation = '${Number(args[1])}' WHERE userID = '${message.author.id}'`);
 		connection.query(`UPDATE stats SET timeLength = '${Number(message.createdTimestamp) + Number(ms('1h'))}' WHERE userID = '${message.author.id}'`);
 
 		const farmEmbed = new discord.RichEmbed()
