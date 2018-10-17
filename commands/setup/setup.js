c7f0439f5f248678d2c9356ecb7baee8e6266db5
PLASMAchicken <plasmachickenmc@gmail.com>
2018-10-17 14:09:48 +0200

    Update setup.js

--- commands/setup/setup.js
+++ commands/setup/setup.js
@@ -1,20 +1,14 @@
-module.exports.run = async (client, message, args, ecoPool) => {
-	ecoPool.getConnection(function(err, connection) {
-		connection.query(`SELECT * FROM stats WHERE userID = '${message.author.id}'`, function(error, results, fields) {
-			if(!results[0].businessName) {
-				connection.release();
-				if (error) throw error;
+module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
+	
+			if(!stats) {
+	
 				message.channel.send(`
 Alright, first things first, What should we call your business? **(?bname <business name>)**
 `);
 			}
 			else {
 				message.reply('You already have a business named **' + results[0].businessName + '** \nIf you would like to reset it do **?breset**');
-				connection.release();
-				if (error) throw error;
 			}
-		});
-	});
 };
 
 module.exports.help = {
