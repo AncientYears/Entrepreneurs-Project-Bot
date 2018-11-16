7e4c83bf68a54a287b9b067bc5d6c532e2404e7a
PLASMAchicken <plasmachickenmc@gmail.com>
2018-11-16 14:34:55 +0100

    #22

--- commands/indevelopment/dbcleaner.js
+++ commands/indevelopment/dbcleaner.js
@@ -12,7 +12,7 @@ const discord = require('discord.js');
 module.exports.run = async (client, message, args, ecoPool, connection, stats) => {
 
 
-	connection.query('SELECT userID, cooldowns FROM businesseco.stats', function(error, users) {
+	connection.query('SELECT userID, cooldowns FROM businesseco.stats', async function(error, users) {
 		if(!args.join(' ')) args = ['30', 'days'];
 		const dbembed = new discord.RichEmbed().setTitle('DB Purge Report');
 		const purger = users.map(data=> {
@@ -26,7 +26,7 @@ module.exports.run = async (client, message, args, ecoPool, connection, stats) =
 		);
 
 
-		dbembed.addField('DB Entrys that did\'nt run a cmd since ' + args.join(' '), purgeable);
+		dbembed.addField('DB Entrys that did\'nt run a cmd since ' + require('ms')(require('ms')(args.join(' ')), { long: true }), await purgeable);
 		dbembed.addField('Total Members Checked', users.length).addField('Members who Qualify for Purge', purger.length).addField('People who wouldnt get purged', (users.length - purger.length));
 		message.channel.send(dbembed);
 	});
