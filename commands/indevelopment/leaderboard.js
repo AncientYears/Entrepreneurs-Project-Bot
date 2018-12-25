5d532df45110d649aa016dd11bc3e67e34ecc885
PLASMAchicken <plasmachickenmc@gmail.com>
2018-12-25 12:49:54 +0100

    linted

--- commands/indevelopment/leaderboard.js
+++ commands/indevelopment/leaderboard.js
@@ -10,7 +10,7 @@ const mysql = require('mysql2'); // eslint-disable-line no-unused-vars
 module.exports.run = async (client, message, args, ecoPool) => {
 	const [data] = await ecoPool.query('select * from stats ORDER BY cash + bank DESC');
 	const mydata = data.map((d, pos) => ({ cash: (d.cash + d.bank), pos: (pos + 1), userID: d.userID })).find(stats => stats.userID === message.author.id);
-	const topfive = data.slice(0, 5).map((d, pos) => (pos + 1) + '# ' + (client.users.get(d.userID) === undefined ? d.userID : client.users.get(d.userID).tag) + ' with ' + (d.cash + d.bank) + '$' );
+	const topfive = data.slice(0, 5).map((d, pos) => (pos + 1) + '# ' + (client.users.get(d.userID) === undefined ? d.userID : client.users.get(d.userID).tag) + ' with ' + (d.cash + d.bank) + '$');
 	const embed = new discord.RichEmbed().setTitle('LeaderBoard').setColor('RANDOM').addField('Your Ranking', `Position: #${mydata.pos}\nCash: ${mydata.cash}$`).addField('Top 5', topfive);
 	message.channel.send(embed);
 };
