2bb08c4c66823f60bf89487c9545a2725ef25ae8
PLASMAchicken <plasmachickenmc@gmail.com>
2019-01-26 14:46:42 +0100

    handled errrors

--- commands/banking/deposit.js
+++ commands/banking/deposit.js
@@ -1,8 +1,9 @@
 module.exports.run = (client, message, args, ecoPool, stats) => {
 	const deposit = client.api.deposit(ecoPool, stats, args[0]);
-	if(deposit.error) {
+	if(deposit.status != 200) {
+		if(deposit.error === 'zumza-NaN') return message.channel.send(`**'${deposit.NaN || 'null'}'** is not a Valid Number!\n**${client.prefix}${this.help.usage}**`);
+		if(deposit.error === 'zumza-notEnoughMoney') return message.channel.send(`You do not eneugh money inside of your bank!\nYou need ${deposit.missing}$ more!`);
 		return message.channel.send(`This command failed because of \`${deposit.error}\`\n\`\`\`${require('util').inspect(deposit)}\`\`\``);
-		// TO-DO: Fancy Error Handler
 	}
 
 	message.channel.send(`You have successfully deposited **${deposit.cost}** into your bank! \nYou have **${Number(deposit.stats.cash) - Number(deposit.cost)}** cash left!`);
--- commands/banking/withdraw.js
+++ commands/banking/withdraw.js
@@ -1,8 +1,9 @@
 module.exports.run = async (client, message, args, ecoPool, stats) => {
 	const withdraw = client.api.withdraw(ecoPool, stats, args[0]);
-	if(withdraw.error) {
+	if(withdraw.status != 200) {
+		if(withdraw.error === 'zumza-NaN') return message.channel.send(`**'${withdraw.NaN || 'null'}'** is not a Valid Number!\n**${client.prefix}${this.help.usage} <amount>**`);
+		if(withdraw.error === 'zumza-notEnoughMoney') return message.channel.send(`You do not eneugh money inside of your bank!\nYou need ${withdraw.missing}$ more!`);
 		return message.channel.send(`This command failed because of \`${withdraw.error}\`\n\`\`\`${require('util').inspect(withdraw)}\`\`\``);
-		// TO-DO: Fancy Error Handler
 	}
 
 	message.channel.send(`You have successfully withdrawn **${Number(withdraw.cost)}** from your bank! \nYou have **${Number(withdraw.stats.bank) - Number(withdraw.cost)}** left inside your bank!`);
@@ -10,6 +11,7 @@ module.exports.run = async (client, message, args, ecoPool, stats) => {
 
 module.exports.help = {
 	name: 'withdraw',
+	usage: 'withdraw <amount>',
 	aliases: ['wd', 'with'],
 	hideinhelp: false,
 };
