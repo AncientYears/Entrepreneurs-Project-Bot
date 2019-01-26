aa70929b872c94c3372d83f9cc58d7a041669a5c
PLASMAchicken <plasmachickenmc@gmail.com>
2019-01-26 14:26:35 +0100

    linted

--- commands/setup/getrole.js
+++ commands/setup/getrole.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args, ecoPool, stats) => {
 	if(!message.member) return message.channel.send('This is not a Guild!');
-	if(message.member.roles.some(role => role.name === 'Entrepreneur-zumza')) return message.channel.send('You already have the rule O.o!').then(msg => msg.delete(5000));;
+	if(message.member.roles.some(role => role.name === 'Entrepreneur-zumza')) return message.channel.send('You already have the rule O.o!').then(msg => msg.delete(5000));
 	if(!stats || !stats.businessName) return message.reply(`Name your business first using **${client.prefix}bname**!`);
 	if(!stats || !stats.businessType) return message.reply(`Select your business type using **${client.prefix}btype**`);
 	if(!stats.businessLocation) {return message.reply(`Select your business type using **${client.prefix}blocate**`);}
