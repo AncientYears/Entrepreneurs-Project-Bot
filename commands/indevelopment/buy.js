b4daa68ab461ca84aa32f6adfbbe672a7ec273da
PLASMAchicken <plasmachickenmc@gmail.com>
2019-01-27 20:50:49 +0100

    Update buy.js

--- commands/indevelopment/buy.js
+++ commands/indevelopment/buy.js
@@ -1,23 +1,42 @@
+const discord = require('discord.js');
+
 module.exports.run = async (client, message, args, ecoPool, stats) => {
-	const buy = client.api.buy(ecoPool, stats, args[0], args[1]);
-	if(buy.error) {
-		return message.channel.send(`This command failed because of \`${buy.error}\`\n\`\`\`${require('util').inspect(buy)}\`\`\``);
-		// TO-DO: Fancy Error Handler
-	}
-	message.channel.send('You succesfully bought ' + buy.bought + ' for ' + buy.cost);
-	// TO-DO: Fancy Error Succesfull handler
-/*
 	if(!args[0]) {
 		const categoryEmbed = new discord.RichEmbed()
 			.setAuthor('Categories', message.author.displayAvatarURL)
 			.setDescription(`**Farm**
 							- Buy all your farm supplies/upgrades here!
-
-		 					**Coming Soon**
+		
+							**Coming Soon**
 							- Coming soon, this is format test`)
-			.setFooter(client.prefix + 'buy <category> to view a category');
+
+			.setFooter(client.format('<prefix>buy <category> to view a category'));
 		return message.channel.send(categoryEmbed);
 	}
+	else if (args[0] === 'farm') {
+		const farmEmbed = new discord.RichEmbed()
+			.setAuthor('Farm', message.author.displayAvatarURL)
+			.setDescription(`**potato** - 1$ / 1
+							 - Cheap crop, not the most profitable though.`)
+
+			.setFooter(client.format('<prefix>buy <item> <amount> to purchase an item'));
+		return message.channel.send(farmEmbed);
+	}
+
+	const buy = client.api.buy(ecoPool, stats, args[0], args[1]);
+	if(buy.error) {
+		return message.channel.send(`This command failed because of \`${buy.error}\`\n\`\`\`${require('util').inspect(buy)}\`\`\``);
+		// TO-DO: Fancy Error Handler
+	}
+	const buyEmbed = new discord.RichEmbed()
+		.setAuthor('Plant', message.author.displayAvatarURL)
+		.setDescription(`Successfully bought **${buy.bought[0]} ${buy.bought[1]}**!\nThis has cost you: **${buy.cost}$**`)
+		.setColor('GREEN');
+	message.channel.send(buyEmbed);
+
+
+/*
+
 	const tobuy = args[0].toLowerCase();
 	if (tobuy === 'farm') {
 		if(stats.businessType !== 'farm') return message.channel.send('Sorry, you do not have a farm! \nYou have a **' + stats.businessType + '**');
