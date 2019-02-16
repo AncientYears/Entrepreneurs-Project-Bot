360cbd4685fa9032b73a34d0c338b9559d969b23
PLASMAchicken <plasmachickenmc@gmail.com>
2019-02-16 13:19:15 +0100

    Update suggest.js

--- commands/indevelopment/suggest.js
+++ commands/indevelopment/suggest.js
@@ -9,7 +9,7 @@ const discord = require('discord.js');
  * @param {Object} stats
  */
 module.exports.run = async (client, message, args) => {
-	const suggest = new discord.RichEmbed().setTitle('Suggestion').setDescription(args.join(' ')).setColor('RANDOM').setFooter(message.author.id);
+	const suggest = new discord.RichEmbed().setDescription(args.join(' ')).setColor('RANDOM').setAuthor('Suggestion', client.user.displayAvatarURL, `https://discordapp.com/users/${message.author.id}`);
 	const hook = new discord.WebhookClient('546299239639154698', 'XRaxzclwO0rr07Mu2xXlkpjCtcdWByG6Q8ZxzAvxKF_2GJMWaOxJoJ4HV0R4RCP478bv');
 	hook.send({ embeds: [suggest], username: message.author.tag, avatarURL: message.author.displayAvatarURL, disableEveryone: true });
 	message.channel.send('Okay thank you, Suggestion Sent!');
@@ -19,5 +19,5 @@ module.exports.help = {
 	name: 'suggest',
 	usage: '<prefix>suggest <info>',
 	hideinhelp: false,
-	cooldown: '10 ms',
+	cooldown: '10 min',
 };
