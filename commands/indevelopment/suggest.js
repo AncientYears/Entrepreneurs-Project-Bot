b70c50fa38705e8276cb16c14b431aa2281df7ab
PLASMAchicken <plasmachickenmc@gmail.com>
2019-02-16 13:57:15 +0100

    Add Reaction to Suggest

--- commands/indevelopment/suggest.js
+++ commands/indevelopment/suggest.js
@@ -11,7 +11,9 @@ const discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
 	const suggest = new discord.RichEmbed().setDescription(args.join(' ')).setColor('RANDOM').setAuthor('Suggestion', client.user.displayAvatarURL, `https://discordapp.com/users/${message.author.id}`);
 	const hook = new discord.WebhookClient('546299239639154698', 'XRaxzclwO0rr07Mu2xXlkpjCtcdWByG6Q8ZxzAvxKF_2GJMWaOxJoJ4HV0R4RCP478bv');
-	hook.send({ embeds: [suggest], username: message.author.tag, avatarURL: message.author.displayAvatarURL, disableEveryone: true });
+	const msg = await hook.send({ embeds: [suggest], username: message.author.tag, avatarURL: message.author.displayAvatarURL, disableEveryone: true });
+	await client.guilds.get('490999695422783489').channels.get('503618118166511626').messages.get(msg.id).react(client.emojis.find(emoji => emoji.name == 'zumzaAgree').id);
+	await client.guilds.get('490999695422783489').channels.get('503618118166511626').messages.get(msg.id).react(client.emojis.find(emoji => emoji.name == 'zumzaDisagree').id);
 	message.channel.send('Okay thank you, Suggestion Sent!');
 };
 
@@ -19,5 +21,5 @@ module.exports.help = {
 	name: 'suggest',
 	usage: '<prefix>suggest <info>',
 	hideinhelp: false,
-	cooldown: '10 min',
+	cooldown: '10 ms',
 };
