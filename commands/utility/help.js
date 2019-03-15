cfef642b86b44c8a26675c3362f7458b52ce6cf0
PLASMAchicken <plasmachickenmc@gmail.com>
2019-03-15 15:50:30 +0100

    !help category support

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -21,21 +21,36 @@ module.exports.run = async (client, message, args) => {
 	}
 	const cmd = args.join(' ').toLowerCase();
 	const command = client.commands.get(cmd) || client.commands.find(commanda => commanda.help.aliases && commanda.help.aliases.includes(cmd));
-	if (!command) return message.reply('that\'s not a valid command!');
-	const helpcmd = [];
-	helpcmd.push(`**Name:** ${command.help.name}`);
-	if (command.help.category) helpcmd.push(`**Category:** ${command.help.category}`);
-	if (command.help.aliases) helpcmd.push(`**Aliases:** [${command.help.aliases.join(', ')}]`);
-	if (command.help.description) helpcmd.push(`**Description:** ${command.help.description}`);
-	if (command.help.usage) helpcmd.push(`**Usage:** ${client.format(command.help.usage)}`);
-	if (command.help.cooldown) helpcmd.push(`**Cooldown:** ${ms(ms(command.help.cooldown), { long : true })}`);
-	message.channel.send(helpcmd, { split: true });
-
+	const category = client.commands.filter(commanda => commanda.help.category && commanda.help.category.toLowerCase() == cmd);
+	if (!command && !category.size) return message.reply('that\'s not a valid command/category!');
+	if(command) {
+		const helpcmd = [];
+		helpcmd.push(`**Name:** ${command.help.name}`);
+		if (command.help.category) helpcmd.push(`**Category:** ${command.help.category}`);
+		if (command.help.aliases) helpcmd.push(`**Aliases:** [${command.help.aliases.join(', ')}]`);
+		if (command.help.description) helpcmd.push(`**Description:** ${command.help.description}`);
+		if (command.help.usage) helpcmd.push(`**Usage:** ${client.format(command.help.usage)}`);
+		if (command.help.cooldown) helpcmd.push(`**Cooldown:** ${ms(ms(command.help.cooldown), { long : true })}`);
+		message.channel.send(helpcmd, { split: true });
+	}
+	if(category.size) 	{
+		return message.author.send(
+			category.map(props => props.help.hideinhelp ? '' : `**Command: ${props.help.name}**\n${props.help.category ? `\tCategory: ${props.help.category}\n` : '' }${props.help.description ? `\tDescription: ${props.help.description}\n` : '' }${props.help.usage ? `\tUsage: ${client.format(props.help.usage)}\n` : '' }${props.help.aliases ? `\tAliases: [ ${props.help.aliases.join(', ')} ]\n` : '' }`).filter(data => data !== '').join(' ')
+			, { split: { char: '\n\n' } })
+			.then(() => {
+				if (message.channel.type === 'dm') return;
+				message.reply('I\'ve sent you a DM with all my commands!');
+			})
+			.catch(error => {
+				message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+				if(error.code != 50007) throw error;
+			});
+	}
 };
 
 module.exports.help = {
 	name: 'help',
 	description: 'Shows all the commands',
-	usage: '<prefix>help or <prefix>help <command>',
+	usage: '<prefix>help or <prefix>help <command||category>',
 	aliases: '',
 };
