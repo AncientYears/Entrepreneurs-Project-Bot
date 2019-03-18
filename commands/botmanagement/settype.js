1a7b5e1f1ebda8a3bb882444f0ce47f10e97e83b
PLASMAchicken <plasmachickenmc@gmail.com>
2019-03-18 20:24:51 +0100

    Create settype.js

---
+++ commands/botmanagement/settype.js
@@ -0,0 +1,34 @@
+const { Client, Message } = require('discord.js');
+const { Pool } = require('mysql2');
+/**
+ * @param {Client} client - Discord.js Client
+ * @param {Message} message - Discord.js Message
+ * @param {Array} args - Array with parsed args
+ * @param {Pool} database - DataBase
+ */
+module.exports.run = async (client, message, args, database) => {
+	let user = message.mentions.users.first() || client.users.get(args[0]);
+	let type = undefined;
+	if(user) {
+		type = args[1];
+	}
+	else {
+		user = message.author;
+		type = args[0];
+
+	}
+	const stats = await client.api.getStats(message.author.id, database).then(data => data.data);
+	stats.business.type = type;
+	database.query(`UPDATE stats SET business = '${JSON.stringify(stats.business)}' WHERE userID = '${user.id}'`);
+
+
+};
+
+
+module.exports.help = {
+	name: 'settype',
+	description: 'settype',
+	usage: '<prefix>settype <@|type> <type>',
+	hideinhelp: true,
+	requires: ['botowner'],
+};
