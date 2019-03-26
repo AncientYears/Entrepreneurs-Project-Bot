9b613a54537320ef3421abb68d5298789fd475ca
PLASMAchicken <plasmachickenmc@gmail.com>
2019-03-26 20:46:58 +0100

    If no viewmarket args provoided give out all
    fixes #38

--- commands/indevelopment/viewmarket.js
+++ commands/indevelopment/viewmarket.js
@@ -1,17 +1,17 @@
 const Discord = require('discord.js');
 /**
  * Command
- * @param {discord.Client} client
- * @param {discord.Message} message
- * @param {Array} args
- * @param {*} ecoPool
- * @param {Object} stats
+ *
+ * @param {Discord.Client} client - Discord.js Client
+ * @param {Discord.Message} message - Discord.js Message
+ * @param {Array} args - Args
+ * @param {*} database - mysql2/promise DataBase
  */
 module.exports.run = async (client, message, args, database) => {
 	const [data] = await database.query('SELECT userID,market FROM businesseco.stats;');
 	const viewmarket = transformMarket(data);
 	let formatted = viewmarket
-		.filter(i => i.type === args.join(' '))
+		.filter(i => i.type === args.join(' ') || !args.join(' '))
 		.map(i => `${i.id}:\t\t${i.amount}x ${i.type} | ${i.price}$`);
 
 
