ce7249eca4018f0b4010294d470c1fc330e5c783
PLASMAchicken <plasmachickenmc@gmail.com>
2019-04-17 21:21:14 +0200

    Update parseStats.js

--- api/parseStats.js
+++ api/parseStats.js
@@ -1,10 +1,12 @@
 module.exports = (stats) => {
-	process.emitWarning('Api#parseStats: use Api#getStats instead', 'DeprecationWarning');
-	if(!stats) stats = {};
+	if(!stats.business) stats.business = {};
+	if(!stats.business.name) stats.business.name = '';
+	if(!stats.business.type) stats.business.type = '';
+	if(!stats.business.location) stats.business.location = '';
 	if(!stats.cooldowns) stats.cooldowns = {};
 	if(!stats.stocks) stats.stocks = {};
 	if(!stats.creation) stats.creation = {};
-	if(!stats.business) stats.business = {};
+	if(!stats.market) stats.market = {};
 	return { 'status': 200, 'data': stats };
 };
 
